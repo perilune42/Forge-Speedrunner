58b58e0a5762990c646fb6581101a360ec543f54
perilune42 <50865458+perilune42@users.noreply.github.com>
2026-01-14 00:30:51 -0800

    jump WIP

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -19,11 +19,21 @@ public class PlayerMovement : DynamicEntity
         
     }
 
-    
+    // MoveInput => (every real frame) bool holdingRight = true;
+    // CheckMove => (every fixed frame) move right if holdingRight
 
     public void MoveInput(CallbackContext context)
     {
         Vector2 inputVec = context.action.ReadValue<Vector2>();
+        Debug.Log(inputVec);
+        float xinput = inputVec.x;
+        Velocity = new Vector2(xinput * Speed, Velocity.y);
+
+    }
+
+    public void JumpInput(CallbackContext context)
+    {
+        bool jumpInput = context.action.WasPressedThisFrame();
         Debug.Log(inputVec);
         float xinput = inputVec.x;
         Velocity = new Vector2(xinput * Speed, Velocity.y);

928152608057be3563ff73ca4224682b396c1ffb
perilune42 <50865458+perilune42@users.noreply.github.com>
2026-02-05 15:59:06 -0800

    some forcefield adjustments

--- Assets/Scripts/Game/Stat.cs
+++ Assets/Scripts/Game/Stat.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public enum StatSource
 {
-    JumpGravityMult, ClimbGravityMult, GrappleGravityMult
+    JumpGravityMult, ClimbGravityMult, GrappleGravityMult, ForceFieldGravityMult
 }
 
 [Serializable]

6dabffba83e32316ef79db6411dd035b34bb4bad
perilune42 <50865458+perilune42@users.noreply.github.com>
2026-02-05 13:34:59 -0800

    import sprites and added button press

--- Assets/Scripts/Map/Entities/Button.cs
+++ Assets/Scripts/Map/Entities/Button.cs
@@ -6,6 +6,20 @@ public class Button : Entity
     public override bool IsSolid => false;
     public List<ActivatableEntity> LinkedEntities = new();
 
+    [SerializeField] Sprite unpressedSprite, pressedSprite;
+
+    SpriteRenderer sr;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        sr = GetComponent<SpriteRenderer>();
+        if (sr == null)
+        {
+            sr = GetComponentInChildren<SpriteRenderer>();
+        }
+    }
+
     public override void OnCollide(DynamicEntity de, Vector2 normal)
     {
         base.OnCollide(de, normal);
@@ -13,5 +27,6 @@ public class Button : Entity
         {
             entity.OnActivate();
         }
+        sr.sprite = pressedSprite;
     }
 }

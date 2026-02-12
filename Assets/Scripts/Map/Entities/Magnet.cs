bea640fc9aa888ef41609fad9e62d9ea53ff8cf8
NickMitroff <nickmitroff@gmail.com>
2026-02-11 19:29:38 -0800

    Particles and better hitboxes for magnets

--- Assets/Scripts/Map/Entities/Magnet.cs
+++ Assets/Scripts/Map/Entities/Magnet.cs
@@ -1,4 +1,5 @@
 using System;
+using Unity.Collections;
 using UnityEngine;
 
 public class Magnet : Entity
@@ -13,18 +14,69 @@ public class Magnet : Entity
     private Collider2D playerCollider;
     [SerializeField] private int cooldown;
     private BoxCollider2D bigBeamColl;
+    private BoxCollider2D coll;
     private int curCooldown;
-
+    [SerializeField] private ParticleSystem particle, particleActive;
+    [SerializeField] private float particleSpeed, particleSpeedActive;
+    private bool active;
+    
     private void Start()
     {
         magnetBeam.transform.localScale = new Vector3(beamRange, beamWidth, 0);
-        BoxCollider2D coll = gameObject.AddComponent<BoxCollider2D>();
+        coll = gameObject.AddComponent<BoxCollider2D>();
         BoxCollider2D beamColl = magnetBeam.GetComponent<BoxCollider2D>();
         coll.size = new Vector2(beamColl.size.x * beamRange, beamColl.size.y * beamWidth);
-        coll.offset = beamColl.offset;
+        coll.offset = new Vector2(beamColl.offset.x * beamRange, beamColl.offset.y * beamWidth);
         bigBeamColl = beamColl;
-        bigBeamColl.size = new Vector2(bigBeamColl.size.x + 1f, bigBeamColl.size.y + 1f);
+        bigBeamColl.size = new Vector2(bigBeamColl.size.x, bigBeamColl.size.y + 1f);
         Hitbox = coll;
+
+        InitializeParticles(particle, particleSpeed);
+        InitializeParticles(particleActive, particleSpeedActive);
+        particleActive.gameObject.SetActive(false);
+    }
+
+
+    private void InitializeParticles(ParticleSystem particle, float speed)
+    {
+        particle.transform.localPosition = new Vector3(coll.size.x * beamWidth * 2, 0, 0);
+        var mainModule = particle.main;
+        var sr = mainModule.startRotation;
+        sr.constant = -transform.eulerAngles.z * Mathf.Deg2Rad;
+        mainModule.startRotation = sr;
+        
+        SetParticleSpeed(particle, speed);
+
+        var ssy = mainModule.startSizeY;
+        ssy = coll.size.y * transform.localScale.y;
+        mainModule.startSizeY = ssy;
+    }
+
+    private void SetParticleSpeed(ParticleSystem particle, float speed)
+    {
+        var mainModule = particle.main;
+
+        var ss = mainModule.startSpeed;
+        ss.constant = speed;
+        mainModule.startSpeed = ss;
+
+        var sl = mainModule.startLifetime;
+        sl.constant = coll.size.x * transform.localScale.x / mainModule.startSpeed.constant;
+        mainModule.startLifetime = sl;
+    }
+
+    private void Activate()
+    {
+        active = true;
+        particle.gameObject.SetActive(false);
+        particleActive.gameObject.SetActive(true);
+    }
+
+    private void Deactivate()
+    {
+        active = false;
+        particle.gameObject.SetActive(true);
+        particleActive.gameObject.SetActive(false);
     }
 
     protected override void FixedUpdate()
@@ -32,11 +84,14 @@ public class Magnet : Entity
         base.FixedUpdate();
         if (playerMovement != null && curCooldown <= 0)
         {
+            if (!active) Activate();
+            
             Suck();
             if (!playerCollider.IsTouching(bigBeamColl))
             {
                 playerMovement = null;
                 curCooldown = 0;
+            if (active) Deactivate();
             }
         }
         else
@@ -63,7 +118,6 @@ public class Magnet : Entity
         if (true /*!(newVelocity.magnitude > playerMovement.Velocity.magnitude && newVelocity.magnitude > maxSuckSpeed)*/)
         {
             playerMovement.Velocity = newVelocity;
-            Debug.Log("sucking");
         }
 
         if (Vector2.Distance(playerPos, transform.position) < minRange)
@@ -71,6 +125,7 @@ public class Magnet : Entity
             curCooldown = cooldown;
             playerMovement.Velocity *= speedDropoff;
             playerMovement = null;
+            if (active) Deactivate();
         }
     }
 }

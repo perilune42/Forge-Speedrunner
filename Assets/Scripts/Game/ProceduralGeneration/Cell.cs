78365d0df9a94d5654c1a04b4f32314eb65f0c08
Yoav Gutman <ygutman@ucsd.edu>
2026-02-14 21:47:25 -0800

    removed unused bools from Cell.

--- Assets/Scripts/Game/ProceduralGeneration/Cell.cs
+++ Assets/Scripts/Game/ProceduralGeneration/Cell.cs
@@ -3,17 +3,9 @@ public struct Cell
 {
     public Room room{get; private set;}
     public Vector2Int offset {get; private set;} // from start
-    bool up   ;
-    bool down ;
-    bool left ;
-    bool right;
     public Cell(Room room, Vector2Int offset)
     {
         this.room = room;
         this.offset = offset;
-        this.up    = false;
-        this.down  = false;
-        this.left  = false;
-        this.right = false;
     }
 }

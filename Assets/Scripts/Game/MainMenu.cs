e44f9021d00d8a8b97d252bffb04f3a3bc646554
AnApostrophe <64292111+AnApostrophe@users.noreply.github.com>
2026-02-24 21:45:30 -0800

    Main Menu Buttons

--- Assets/Scripts/Game/MainMenu.cs
+++ Assets/Scripts/Game/MainMenu.cs
@@ -1,9 +1,15 @@
+using FMODUnity;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
     public static bool GenerateNewMap;
+
+    [Header("Editor Refs")]
+    [SerializeField] private GameObject mainMenuLayoutGroup;
+    [SerializeField] private GameObject startGameLayoutGroup;
 
     private void Awake()
     {
@@ -12,7 +18,43 @@ public class MainMenu : MonoBehaviour
 
     public void StartGame()
     {
+        mainMenuLayoutGroup.SetActive(false);
+        startGameLayoutGroup.SetActive(true);
+    }
+
+    public void StartGameDifficulty(int difficulty)
+    {
+        // Set initial timer / scaling stuff
+
         GenerateNewMap = true;
-        SceneManager.LoadScene("World");
+        StartCoroutine(LoadSceneAsync());
+    }
+
+    public void BackFromStartGame()
+    {
+        mainMenuLayoutGroup.SetActive(true);
+        startGameLayoutGroup.SetActive(false);
+    }
+
+    public void CreditsButton()
+    {
+        // Credits
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    IEnumerator LoadSceneAsync()
+    {
+        // Start fade
+
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("World");
+
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
     }
 }

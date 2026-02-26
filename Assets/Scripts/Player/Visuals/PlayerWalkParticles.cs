413541aba0559a6e7e128339558dddb3a4f3772b
NickMitroff <nickmitroff@gmail.com>
2026-02-25 17:21:03 -0800

    Player walk particles + moved player visual scripts into their own folder

---
+++ Assets/Scripts/Player/Visuals/PlayerWalkParticles.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class PlayerWalkParticles : MonoBehaviour
+{
+    [SerializeField] private PlayerMovement playerMovement;
+    [SerializeField] private ParticleSystem particle;
+    [SerializeField] private float baseStrength;
+
+    private void Update()
+    {
+        float strength = 1f;
+        if (playerMovement.State != BodyState.OnGround) strength = 0;
+        else if (playerMovement.MoveDir.x == 0) strength = 0;
+        var emission = particle.emission;
+        var minMaxCurve = emission.rateOverDistance;
+        minMaxCurve.constantMin = strength * baseStrength;
+        minMaxCurve.constantMax = strength * baseStrength * 2f;
+        emission.rateOverDistance = minMaxCurve;
+        var shape = particle.shape;
+        shape.scale = new Vector3(playerMovement.MoveDir.x, 1, 1);
+
+    }
+}
